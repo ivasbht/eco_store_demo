07cc311fd156df3905d2866a1cbe42748941d161
abhilash_aima <abhilash_aima@thbs.com>
2024-05-12 17:13:51 +0530

    adding search Bar in home page

--- lib/UI/application_screen/home_screen/home_screen.dart
+++ lib/UI/application_screen/home_screen/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:eco_store_demo/UI/widgets_collection/custom_text/custom_text.dart';
+import 'package:eco_store_demo/UI/widgets_collection/custom_textfield_widget/custom_textfield_widget.dart';
+import 'package:eco_store_demo/UI/widgets_collection/mixins/size_mixin/size_mixin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,21 +11,49 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SizeMixin {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    initializaSize(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: EdgeInsets.only(
+          top: screenHeight * 0.025,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            CustomText(
-              text: "HomePage",
-            )
+            _buildSearchBar(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSearchBar({
+    TextEditingController? searchController,
+  }) {
+    return Container(
+      width: screenWidth * 0.9,
+      child: CustomTextfieldWidget(
+        controller: searchController,
+        fillColor: Colors.blueAccent.shade100,
+        filled: true,
+        hintText: "Search Your Item Here....",
+        hintStyle: TextStyle(color: Colors.white54),
+        style:TextStyle(color: Colors.white),
+        suffixIcon: Icon(
+          Icons.search,
+          size: 30,
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
       ),
     );
   }
